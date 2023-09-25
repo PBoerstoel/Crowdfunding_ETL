425355bbfc97e8163580305608fedc77c213ef0a
oliverkisza <64221165+oliverkisza@users.noreply.github.com>
2023-09-25 19:03:24 -0400

    Updated crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,34 +1,43 @@
 -- contacts table
 CREATE TABLE contacts (
-    contact_id SERIAL PRIMARY KEY,
+    contact_id INT PRIMARY KEY,
     first_name VARCHAR,
     last_name VARCHAR,
     email VARCHAR UNIQUE
 );
--- campaign table
-CREATE TABLE campaign (
-    cf_id SERIAL PRIMARY KEY,
-    contact_id INT REFERENCES contacts(contact_id),
-    company_name VARCHAR,
-    description TEXT,
-    goal VARCHAR,
-    pledged VARCHAR,
-    outcome VARCHAR,
-    backers_count INT,
-    country VARCHAR,
-    currency VARCHAR,
-    launched_date DATE,
-    end_date DATE
-);
 
 -- category table
 CREATE TABLE category (
-    category_id SERIAL PRIMARY KEY,
+    category_id VARCHAR PRIMARY KEY,
     category VARCHAR UNIQUE
 );
 
 -- subcategory table
 CREATE TABLE subcategory (
-    subcategory_id SERIAL PRIMARY KEY,
+    subcategory_id VARCHAR PRIMARY KEY,
     subcategory VARCHAR UNIQUE
 );
+
+-- campaign table
+CREATE TABLE campaign (
+    cf_id INT PRIMARY KEY,
+    contact_id INT REFERENCES contacts(contact_id),
+    company_name VARCHAR,
+    description VARCHAR,
+    goal DECIMAL,
+    pledged DECIMAL,
+    outcome VARCHAR,
+    backers_count INT,
+    country VARCHAR,
+    currency VARCHAR,
+    launched_date DATE,
+    end_date DATE,
+	category_id VARCHAR REFERENCES category(category_id),
+	subcategory_id VARCHAR REFERENCES subcategory(subcategory_id)
+);
+
+-- table checks
+SELECT * FROM contacts
+SELECT * FROM category
+SELECT * FROM subcategory
+SELECT * FROM campaign	
